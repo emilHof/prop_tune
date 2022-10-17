23da14d32610b33b3ca71a8f00b1cb588a56d0c0
emilHof <95590295+emilHof@users.noreply.github.com>
2022-10-17 13:25:39 -0400

    Added tests and timed tests

--- prop_tune/src/lib.rs
+++ prop_tune/src/lib.rs
@@ -9,6 +9,65 @@ fn eval(a: bool, b: bool, c: bool, d: bool) -> bool {
     }
 }
 
+fn test_belt() {
+    let cases = vec![true, false];
+
+    for &a in cases.iter() {
+        for &b in cases.iter() {
+            for &c in cases.iter() {
+                for &d in cases.iter() {
+                    for &e in cases.iter() {
+                        assert_eq!(
+                            (e && !(c && a)) && ((a && (b || !c)) && !((d || !b) && !(a && c))),
+                            simplify!(
+                                (e && !(c && a)) && ((a && (b || !c)) && !((d || !b) && !(a && c)))
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn t1() {
+    let cases = vec![true, false];
+
+    for &a in cases.iter() {
+        for &b in cases.iter() {
+            for &c in cases.iter() {
+                for &d in cases.iter() {
+                    for &e in cases.iter() {
+                        if simplify!(
+                            ((e || !(a && !(b && c))) && !(c && a)) 
+                            && ((a && (b || !c)) && !((d || !b) && !(a && c)))
+                        ) { true } else { false };
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn t2() {
+    let cases = vec![true, false];
+    
+    for &a in cases.iter() {
+        for &b in cases.iter() {
+            for &c in cases.iter() {
+                for &d in cases.iter() {
+                    for &e in cases.iter() {
+                        if 
+                        ((e || !(a && !(b && c))) && !(c && a)) 
+                        && ((a && (b || !c)) && !((d || !b) && !(a && c)))
+                        { true } else { false };
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod test_lib {
     use super::*;
@@ -16,5 +75,32 @@ mod test_lib {
     #[test]
     fn test_macro() {
         println!("{}", eval(true, true, false, false))
+    }
+
+    #[test]
+    fn test_equiv() {
+        test_belt()
+    }
+
+    #[test]
+    fn test_t1() {
+        use std::time::Instant;
+        
+        let now = Instant::now();
+
+        (0..100_000_0).for_each(|_| t1());
+
+        println!("{:.2?}", now.elapsed());
+    }
+
+    #[test]
+    fn test_t2() {
+        use std::time::Instant;
+        
+        let now = Instant::now();
+
+        (0..100_000_0).for_each(|_| t2());
+
+        println!("{:.2?}", now.elapsed());
     }
 }
